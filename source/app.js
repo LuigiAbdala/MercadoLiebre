753a1ec9219cc0340670e143132ea1be27dae9fc
LuigiAbdala <luigiabdala12@gmail.com>
2022-10-19 20:48:38 -0300

    Subiendo archivos

---
+++ source/app.js
@@ -0,0 +1,15 @@
+const path =require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+
+app.listen(port,() => console.log('Server On'));
+
+const public = path.resolve(__dirname, '../public');
+app.use(express.static(public));
+
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views/home.html')))
+
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views/login.html')))
+
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views/register.html')))
